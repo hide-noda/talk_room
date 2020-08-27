22aeeb183d6066765b54cf735cb342d92bd2cba0
hide-noda <hideyoshi.noda@gmail.com>
2020-08-27 18:54:42 +0900

    バリデーションの実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  KANA_RAGEX = /\A[ァ-ヶー－]+\z/.freeze
+  with_options presence: true do
+    validates :family_name
+    validates :last_name
+    validates :family_name_kana, format: {with: KANA_RAGEX}
+    validates :last_name_kana, format: {with: KANA_RAGEX}
+  end
+
 end
